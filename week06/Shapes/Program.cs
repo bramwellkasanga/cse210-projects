bb46db3d48d5b78f80cc69c51643590b7fa04baa
bramwellkasanga <bramwellk165@gmail.com>
2026-02-11 13:11:42 +0200

    commit

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square square = new Square("Blue", 4);
+        Console.WriteLine($"Square color: {square.GetColor()}, area: {square.GetArea()}");
+
+        Rectangle rectangle = new Rectangle("Green", 3, 5);
+        Console.WriteLine($"Rectangle color: {rectangle.GetColor()}, area: {rectangle.GetArea()}");
+
+        Circle circle = new Circle("Red", 2.5);
+        Console.WriteLine($"Circle color: {circle.GetColor()}, area: {circle.GetArea()}");
+
+        List<Shape> shapes = new List<Shape>
+        {
+            square,
+            rectangle,
+            circle,
+            new Square("Yellow", 6)
+        };
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"{shape.GetColor()} shape area: {shape.GetArea()}");
+        }
     }
 }
